dd55e4dd6ba211c1c25aabc5019cd35aa098917e
brokerstir <take1risk@gmail.com>
2018-05-15 09:57:46 -0500

    better word count initial commit

---
+++ word-count-better.py
@@ -0,0 +1,21 @@
+import re # use regular expressions
+from pyspark import SparkConf, SparkContext
+
+def normalizeWords(text):
+    # regex to strip punctuation and put words in lowercase
+    return re.compile(r'\W+', re.UNICODE).split(text.lower())
+
+conf = SparkConf().setMaster("local").setAppName("WordCount")
+sc = SparkContext(conf = conf)
+
+input = sc.textFile("file:///sparkcourse/book.txt")
+words = input.flatMap(normalizeWords)
+wordCounts = words.countByValue()
+
+for word, count in wordCounts.items():
+    cleanWord = word.encode('ascii', 'ignore')
+    if (cleanWord):
+        print(cleanWord.decode() + " " + str(count))
+
+# punctuation is stripped and all words in lowercase
+# still could improve by sorting words by count
